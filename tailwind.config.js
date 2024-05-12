705b79f77756e9ff12a2528c716c2835729f7bd2
希嘉嘉 <hi@xijaja.com>
2024-05-12 22:54:38 +0800

    chore: Update npm dependencies and configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,15 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   darkMode: 'class',
-  content: [
-    './src/**/*.{vue,js,ts,html}',
-  ],
+  content: ['./src/**/*.{vue,js,ts,html}'],
   theme: {
     extend: {}
   },
   // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark', 'cupcake'], // 主题
-    darkTheme: 'dark', // 默认深色主题
+    themes: ['light', 'dark', 'dim'], // 主题
+    darkTheme: 'dim', // 默认深色主题
     base: true, // 是否启用基础样式
     styled: true, // 是否启用daisyUI组件
     utils: true, // 是否启用daisyUI工具
